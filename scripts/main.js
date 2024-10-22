b12bab75fadbd37f59bda4502c1f03de697e5667
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2024-10-22 19:30:54 -0300

    touch end and touch star added in js

--- scripts/main.js
+++ scripts/main.js
@@ -8,17 +8,17 @@ hamburgerMenu.addEventListener("click", () => {
 })
 
 
-
 // carrusel
-
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -37,4 +37,31 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+}
+
+// Detectar deslizamiento en dispositivos móviles
+let startX;
+
+const slideshowContainer = document.querySelector('.slideshow-container');
+
+// Al tocar la pantalla
+slideshowContainer.addEventListener('touchstart', function (e) {
+    startX = e.touches[0].clientX;
+});
+
+// Al soltar el toque
+slideshowContainer.addEventListener('touchend', function (e) {
+    let endX = e.changedTouches[0].clientX;
+    handleSwipe(startX, endX);
+});
+
+function handleSwipe(start, end) {
+    let diffX = start - end;
+    if (Math.abs(diffX) > 50) { // Si el deslizamiento es mayor a 50px
+        if (diffX > 0) {
+            plusSlides(1);  // Desliza a la derecha (próxima imagen)
+        } else {
+            plusSlides(-1); // Desliza a la izquierda (imagen anterior)
+        }
+    }
 }
