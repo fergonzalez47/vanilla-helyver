e9fe9b59cdbe9c82ed4d0537706ac87a64e0eb9b
fergonzalez47 <fernandogonzalezvaldz@gmail.com>
2024-10-26 15:57:19 -0300

    formulario incluyendose

--- scripts/cotiza.js
+++ scripts/cotiza.js
@@ -24,10 +24,22 @@ selectService.addEventListener("change", () => {
 
 
 
-function createElement() {
+function createWindowQuote() {
 
     let fieldset = document.createElement("fieldset");
-    let innerElements = `<legend>Cotización de Ventana</legend>`;
+    let innerElements = `
+    <legend>Cotización de Ventana</legend>
+
+        <div><i>Por favor, ingrese solo el número de las medidas en centímetros (cm), sin incluir el texto "cm".</i></div>
+        </div>
+        <label>
+            <p>Ancho: </p>
+            <input placeholder="Ejemplo: 125 (cm)" type="number" name="window-width" required id="window-width"> Cm
+        </label>
+        <label>
+            <p>Alto: </p> <input placeholder="Ejemplo: 90 (cm)" type="number" name="window-height" required id="window-height"> Cm
+        </label>
+    `;
 
     fieldset.setAttribute("value", param.id);
     return fieldset;
