a9ba2c4ddbe8da99eac1a63b00bea0760aad4417
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2024-11-06 19:50:58 -0300

    transitions

--- scripts/main.js
+++ scripts/main.js
@@ -9,22 +9,38 @@ hamburgerMenu.addEventListener("click", () => {
 
 
 
-// form
-
+//IntersectionObserver
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector(".wf1");
+    const objectsToTransition = document.querySelectorAll(".IOO");
 
     const observer = new IntersectionObserver(
         (entries, observer) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    entry.target.classList.add("form-visible");
+
+                    entry.target.classList.add("visible");
+
+
+
+                    // Seleccionar y animar los elementos hijos con la clase "hidden-content"
+                    const hiddenContents = entry.target.querySelectorAll(".hidden-content");
+                    hiddenContents.forEach(content => {
+                        content.classList.add("visible-content");
+
+                        content.addEventListener("transitionend", () => {
+                            content.classList.remove("hidden-content");
+                        }, { once: true });
+                    });
+
+                    entry.target.addEventListener("transitionend", () => {
+                        entry.target.classList.remove("hidden");
+                    }, { once: true })
                     observer.unobserve(entry.target); // Deja de observar después de la primera aparición
                 }
             });
         },
-        { threshold: 0.1 } // Cambia este valor si quieres que el efecto ocurra antes o después
+        { threshold: 0.1 } // este valor representa el procentaje del elemento mostrandose en pantalla en %. efecto ocurra antes o después
     );
 
-    observer.observe(form);
+    objectsToTransition.forEach(obj => observer.observe(obj));
 });
