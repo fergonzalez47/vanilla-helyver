c6a173c01388ff6dfcbaf944551b823e05789b0a
fergonzalez47 <fernandogonzalezvaldz@gmail.com>
2024-11-09 19:53:49 -0300

    doing the cotizxa

--- scripts/cotiza.js
+++ scripts/cotiza.js
@@ -1,103 +1,115 @@
 let selectService = document.getElementById("service");
-// let displayOption = document.getElementById("display-option");
+const displayOption = document.getElementById("display-option");
 
 
-// selectService.addEventListener("change", () => {
-
-//     let option = selectService.value;
-//     displayOption.innerHTML = "";
-//     switch (option) {
-//         case "ventana":
-//             displayOption.appendChild(createWindowQuote());
-//             console.log("ventana");
-
-//             break;
-
-//         case "puerta":
-//             console.log("puerta");
-//             break;
-
-//         case "espejo":
-//             console.log("espejo");
-//             break;
-
-//         default:
-//             break;
-//     }
-// })
-
-
-
-function createWindowQuote() {
-
-    let fieldset = document.createElement("fieldset");
-    let innerElements = `
-       <div class="cotizacion-ventana">
-    <fieldset>
-        <legend>Cotización de Ventana</legend>
-        <div><i>Por favor, ingrese las medidas en centímetros (cm), sin incluir el texto "cm".</i></div>
-        <label>
-            <p>Ancho:</p>
-            <input placeholder="Ejemplo: 125 (cm)" type="number" name="window-width" required id="window-width"> Cm
-        </label>
-        <label>
-            <p>Alto:</p>
-            <input placeholder="Ejemplo: 90 (cm)" type="number" name="window-height" required id="window-height"> Cm
-        </label>
-    </fieldset>
-
-    <fieldset>
-        <legend><span class="required-element">*</span>Línea Aluminio:</legend>
-        <label>
-            <input type="radio" name="line" required id="line-25" value="line-25"> Línea 25
-        </label>
-        <label>
-            <input type="radio" name="line" required id="line-5000" value="line-5000"> Línea 5000
-        </label>
-    </fieldset>
-</div>`;
-
-    fieldset.innerHTML = innerElements;
-    return fieldset;
-};
-
-
-function createDoorQuote() {
-
-}
-
-function createMirrorQuote() {
-    let fieldset = document.createElement("fieldset");
-    let innerElements = `
-        <div class="cotizacion-espejo">
-
-                    <legend>Cotización de Espejo</legend>
-
-                    <div><i>Por favor, ingrese las medidas en centímetros (cm), sin incluir el texto
-                            "cm".</i></div>
-                    <label>
-                        <p>Ancho:</p>
-                        <input placeholder="Ejemplo: 125 (cm)" type="number" name="window-width" required
-                            id="window-width"> Cm
-                    </label>
-                    <label>
-                        <p>Alto: </p> <input placeholder="Ejemplo: 90 (cm)" type="number" name="window-height" required
-                            id="window-height"> Cm
-                    </label>
-                </fieldset>
-
+function btnAdd() {
+    let fragment = document.createDocumentFragment();
+    let fragmentSelect = document.createDocumentFragment();
+    fragmentSelect.innerHTML = `
                 <fieldset>
-                    <legend><span class="required-element">*</span>Linea Aluminio: </legend>
-                    <label>
-                        <input type="radio" name="line" required id="line-25" value="line-25"> Linea 25
-                    </label>
-                    <label>
-                        <input type="radio" name="line" required id="line-5000" value="line-5000"> Linea 5000
-                    </label>
-    `;
 
+                <legend><span class="required-element">*</span>¿Qué quieres cotizar?</legend>
+                <label>
+                    <select name="service" id="service" required>
+                        <option value="" selected aria-disabled="true" disabled>Elige el producto o servicio...</option>
+                        <option value="celocia">Celocia</option>
+                        <option value="espejo">Espejo</option>
+                        <option value="puerta">Puerta</option>
+                        <option value="ventanaNormal">Ventana</option>
+                        <option value="ventanaProyectante">Ventana Proyectante</option>
+
+                    </select>
+                </label>
+            </fieldset>
+    `;
+    let div = document.createElement("div");
+    div.classList.add("new-service");
+
+    let addBtn = document.createElement("button");
+    addBtn.setAttribute("title", "Añadir nueva cotización");
+    addBtn.id = "add-btn";
+
+    let img = document.createElement("img");
+    img.setAttribute("src", "../images/svg/add-btn.svg");
+    img.setAttribute("alt", "Simbolo de añadir");
+
+    addBtn.appendChild(img);
+
+    addBtn.addEventListener("click", () => {
+        addBtn.remove();
+
+        div.appendChild();
+
+    })
+
+    div.appendChild(addBtn);
+    fragment.appendChild(div);
+
+    return fragment;
 }
 
+
+// function removeService(params) {
+
+// }
+
+
+function createQuoteOptions() {
+    const fragment = document.createDocumentFragment();
+
+
+    const fieldset = document.createElement("fieldset");
+    const legend = document.createElement("legend");
+    legend.innerHTML = `<span class="required-element">*</span>¿Qué quieres cotizar?`;
+
+    const label = document.createElement("label");
+    const select = document.createElement("select");
+    select.name = "service";
+    select.id = "service";
+    select.required = true;
+
+    
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.selected = true;
+    defaultOption.disabled = true;
+    defaultOption.setAttribute("aria-disabled", "true");
+    defaultOption.textContent = "Elige el producto o servicio...";
+
+  
+    const options = [
+        { value: "celocia", text: "Celocia" },
+        { value: "espejo", text: "Espejo" },
+        { value: "puerta", text: "Puerta" },
+        { value: "ventanaNormal", text: "Ventana" },
+        { value: "ventanaProyectante", text: "Ventana Proyectante" }
+    ];
+
+
+    select.appendChild(defaultOption);
+    options.forEach(({ value, text }) => {
+        const option = document.createElement("option");
+        option.value = value;
+        option.textContent = text;
+        select.appendChild(option);
+    });
+
+
+    select.addEventListener("change", (e) => {
+        const closestContainer = e.target.closest(".new-service");
+        closestContainer.innerHTML = ""; 
+        closestContainer.appendChild(select); 
+        closestContainer.appendChild(createBaseQuote(e.target.value));
+    });
+
+ 
+    label.appendChild(select);
+    fieldset.appendChild(legend);
+    fieldset.appendChild(label);
+    fragment.appendChild(fieldset);
+
+    return fragment;
+}
 
 
 
@@ -114,6 +126,7 @@ function createBaseQuote(serviceType) {
 
     };
 
+
     let divContainer = document.createElement("div");
     divContainer.classList.add(`cotizacion-${serviceType}`);
     let fieldset = document.createElement("fieldset");
@@ -124,7 +137,6 @@ function createBaseQuote(serviceType) {
             <legend>Cotización de ${selectedService.name}</legend>`;
 
     if (serviceType === "celocia") {
-        // Agregar opciones adicionales de color, tipo de cristal y grosor
         innerElements += `
         <fieldset>
             <legend>Color:</legend>
@@ -170,7 +182,7 @@ function createBaseQuote(serviceType) {
                 `).join('')}
             </fieldset>
         `;
-            
+
             // opciones adicionales de color, tipo de cristal y grosor
 
             // esto despues lo puedo separar en otra condicional mas si es necesario
@@ -192,12 +204,132 @@ function createBaseQuote(serviceType) {
     return divContainer;
 }
 
-// Ejemplo de uso
-const displayOption = document.getElementById("display-option");
-document.getElementById("service").addEventListener("change", (e) => {
-    displayOption.innerHTML = ""; // Limpia el contenido previo
-    displayOption.appendChild(createBaseQuote(e.target.value));
-});
+displayOption.appendChild(btnAdd());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createBaseQuote(serviceType) {
+
+//     const config = {
+//         celocia: { name: "Celocia", lineOptions: ["Pequeña", "Grande"] },
+//         espejo: { name: "Espejo", lineOptions: false },
+//         puerta: { name: "Puerta", lineOptions: ["Linea 35"] },
+//         ventanaNormal: { name: "Ventana", lineOptions: ["Linea 25", "Linea 5000"] },
+//         ventanaProyectante: { name: "Ventana Proyectante", lineOptions: ["Linea 42"] }
+
+//     };
+
+//     let divContainer = document.createElement("div");
+//     divContainer.classList.add(`cotizacion-${serviceType}`);
+//     let fieldset = document.createElement("fieldset");
+
+//     let selectedService = config[serviceType];
+
+//     let innerElements = `
+//             <legend>Cotización de ${selectedService.name}</legend>`;
+
+//     if (serviceType === "celocia") {
+//         innerElements += `
+//         <fieldset>
+//             <legend>Color:</legend>
+//             <label class="color"><input type="radio" name="color" value="blanco">Blanco <div class="color-option" id="color-blanco"></div></label>
+//             <label class="color"><input type="radio" name="color" value="madera"> Madera <div class="color-option" id="color-madera"></div></label>
+//             <label class="color"><input type="radio" name="color" value="mate"> Mate <div class="color-option" id="color-mate"></div></label>
+//             <label class="color"><input type="radio" name="color" value="Negro"> Negro <div class="color-option" id="color-negro"></div></label>
+//             <label class="color"><input type="radio" name="color" value="titanio"> Titanio <div class="color-option" id="color-titanio"></div></label>
+//         </fieldset>
+//         <fieldset>
+//             <legend>Tamaño:</legend>
+//             <label><input type="radio" name="tamaño" value="grande"> Grande</label>
+//             <label><input type="radio" name="tamaño" value="pequeña"> Pequeña</label>
+//         </fieldset>
+//     `;
+
+
+//     } else {
+
+
+//         // Form Base
+//         innerElements = `
+//             <div><i>Por favor, ingrese las medidas en centímetros (cm), sin incluir el texto "cm".</i></div>
+//             <label>
+//                 <p>Ancho:</p>
+//                 <input placeholder="Ejemplo: 125 (cm)" type="number" name="width" required id="${serviceType}-width"> Cm
+//             </label>
+//             <label>
+//                 <p>Alto: </p> 
+//                 <input placeholder="Ejemplo: 90 (cm)" type="number" name="height" required id="${serviceType}-height"> Cm
+//             </label>
+//         `;
+
+//         // opciones de línea de aluminio solo si aplica
+//         if (selectedService.lineOptions) {
+//             innerElements += `
+//             <fieldset>
+//                 <legend>Línea Aluminio:</legend>
+//                 ${selectedService.lineOptions.map(line => `
+//                     <label>
+//                         <input type="radio" name="line" required value="${line}"> ${line}
+//                     </label>
+//                 `).join('')}
+//             </fieldset>
+//         `;
+            
+//             // opciones adicionales de color, tipo de cristal y grosor
+
+//             // esto despues lo puedo separar en otra condicional mas si es necesario
+//             innerElements += `
+//         <fieldset>
+//             <legend>Color:</legend>
+//             <label class="color"><input type="radio" name="color" value="blanco">Blanco <div class="color-option" id="color-blanco"></div></label>
+//             <label class="color"><input type="radio" name="color" value="madera"> Madera <div class="color-option" id="color-madera"></div></label>
+//             <label class="color"><input type="radio" name="color" value="mate"> Mate <div class="color-option" id="color-mate"></div></label>
+//             <label class="color"><input type="radio" name="color" value="Negro"> Negro <div class="color-option" id="color-negro"></div></label>
+//             <label class="color"><input type="radio" name="color" value="titanio"> Titanio <div class="color-option" id="color-titanio"></div></label>
+//         </fieldset>
+//     `;
+//         }
+
+//     }
+//     fieldset.innerHTML = innerElements;
+//     divContainer.appendChild(fieldset);
+//     return divContainer;
+// }
+
+// // Ejemplo de uso
+
+
+// selectService.addEventListener("change", (e) => {
+//     displayOption.innerHTML = "";
+//     displayOption.appendChild(createBaseQuote(e.target.value));
+// });
 
 
 
