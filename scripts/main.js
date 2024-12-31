561ac538eaafc67db4e8c966ac55b651da4fe39b
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2024-12-31 20:31:09 -0300

    new nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-const hamburgerMenu = document.getElementById("menu");
-const navigation = document.querySelector('.navigation');
+// const hamburgerMenu = document.getElementById("menu");
+// const navigation = document.querySelector('.navigation');
 
 
-hamburgerMenu.addEventListener("click", () => {
-    hamburgerMenu.classList.toggle("open");
-    navigation.classList.toggle("open");
-})
+// hamburgerMenu.addEventListener("click", () => {
+//     hamburgerMenu.classList.toggle("open");
+//     navigation.classList.toggle("open");
+// })
 
 
 
